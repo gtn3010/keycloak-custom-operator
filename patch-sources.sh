28faca21aa937650ed9ceb64cfe7cfa9f1f07363
Nguyen GT <nguyengt@vpbank.com.vn>
2023-04-19 13:39:50 +0700

    add keycloak-core module

--- patch-sources.sh
+++ patch-sources.sh
@@ -18,39 +18,39 @@ function addAnnotation() {
 addAnnotation \
   "protected List<GroupRepresentation> subGroups;" \
   "@io.fabric8.crd.generator.annotation.SchemaFrom(type = org.keycloak.representations.overrides.NoSubGroupsGroupRepresentationList.class)" \
-  $HOME/.m2/keycloak-core/org/keycloak/representations/idm/GroupRepresentation.java
+  target/keycloak-core/org/keycloak/representations/idm/GroupRepresentation.java
 
 addAnnotation \
   "private MultivaluedHashMap<String, ComponentExportRepresentation> components;" \
   "@io.fabric8.crd.generator.annotation.SchemaFrom(type = org.keycloak.representations.overrides.ComponentExportRepresentationMap.class)" \
-  $HOME/.m2/keycloak-core/org/keycloak/representations/idm/RealmRepresentation.java
+  target/keycloak-core/org/keycloak/representations/idm/RealmRepresentation.java
 
 addAnnotation \
   "private MultivaluedHashMap<String, String> config;" \
   "@io.fabric8.crd.generator.annotation.SchemaFrom(type = org.keycloak.representations.overrides.MultivaluedStringStringHashMap.class)" \
-  $HOME/.m2/keycloak-core/org/keycloak/representations/idm/CredentialRepresentation.java
+  target/keycloak-core/org/keycloak/representations/idm/CredentialRepresentation.java
 
 addAnnotation \
   "private MultivaluedHashMap<String, String> config;" \
   "@io.fabric8.crd.generator.annotation.SchemaFrom(type = org.keycloak.representations.overrides.MultivaluedStringStringHashMap.class)" \
-  $HOME/.m2/keycloak-core/org/keycloak/representations/idm/ComponentRepresentation.java
+  target/keycloak-core/org/keycloak/representations/idm/ComponentRepresentation.java
 
 addAnnotation \
   "private MultivaluedHashMap<String, ComponentExportRepresentation> subComponents = new MultivaluedHashMap<>();" \
   "@io.fabric8.crd.generator.annotation.SchemaFrom(type = org.keycloak.representations.overrides.NoSubcomponentsComponentExportRepresentationMap.class)" \
-  $HOME/.m2/keycloak-core/org/keycloak/representations/idm/ComponentExportRepresentation.java
+  target/keycloak-core/org/keycloak/representations/idm/ComponentExportRepresentation.java
 
 addAnnotation \
   "private MultivaluedHashMap<String, String> config = new MultivaluedHashMap<>();" \
   "@io.fabric8.crd.generator.annotation.SchemaFrom(type = org.keycloak.representations.overrides.MultivaluedStringStringHashMap.class)" \
-  $HOME/.m2/keycloak-core/org/keycloak/representations/idm/ComponentExportRepresentation.java
+  target/keycloak-core/org/keycloak/representations/idm/ComponentExportRepresentation.java
 
 addAnnotation \
   "private List<PolicyRepresentation> policies;" \
   "@com.fasterxml.jackson.annotation.JsonIgnore" \
-  $HOME/.m2/keycloak-core/org/keycloak/representations/idm/authorization/ScopeRepresentation.java
+  target/keycloak-core/org/keycloak/representations/idm/authorization/ScopeRepresentation.java
 
 addAnnotation \
   "private List<ResourceRepresentation> resources;" \
   "@com.fasterxml.jackson.annotation.JsonIgnore" \
-  $HOME/.m2/keycloak-core/org/keycloak/representations/idm/authorization/ScopeRepresentation.java
+  target/keycloak-core/org/keycloak/representations/idm/authorization/ScopeRepresentation.java
